cad73e6732b9cc00211cbefda43bc433265a9411
Strahinja Pavićević <pavicevicstrahinja99@gmail.com>
2026-04-21 20:44:16 +0200

    added login form, starting HUGE update adding all database models

--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
+using Sprintra.Services;
+
 namespace Sprintra;
 
 internal static class Program {
   [STAThread]
   static void Main() {
+
+    if (File.Exists("user.dat")) {
+      string encryptedData = File.ReadAllText("user.dat");
+      string username = SecurityService.Unprotect(encryptedData);
+
+      if (!string.IsNullOrEmpty(username)) {
+        //// Moraš napraviti instancu context-a
+        //using var db = new AppDbContext();
+        //var user = db.Employees.FirstOrDefault(u => u.Username == username);
+
+        //if (user != null) {
+        //  CurrentUser.Initialize(user);
+        //}
+      }
+    }
+
     ApplicationConfiguration.Initialize();
-    Application.Run(new Forms.MainForm());
+
+    if (CurrentUser.IsLoggedIn) {
+      Application.Run(new Forms.MainForm());
+    }
+    else {
+      // Application.Run(new Forms.LoginForm()); // Ovo otkomentariši kad napraviš Login prozor
+      Application.Run(new Forms.MainForm());
+    }
   }
 }
---
+++ Services/CurrentUser.cs
@@ -0,0 +1,16 @@
+﻿using Sprintra.Models;
+
+namespace Sprintra.Services;
+
+internal class CurrentUser {
+  public static User? Details { get; private set; }
+  public static bool IsLoggedIn => Details != null;
+
+  public static void Initialize(User user) {
+    Details = user;
+  }
+
+  public static void Logout() {
+    Details = null;
+  }
+}
