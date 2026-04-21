271da54487ceaad4c8f2cda1e776c526e80bb994
Strahinja Pavićević <pavicevicstrahinja99@gmail.com>
2026-04-21 18:56:55 +0200

    Starting to add UI libraries

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -1,12 +1,12 @@
-﻿namespace Sprintra {
-  partial class Form1 {
+﻿namespace Sprintra.Forms {
+  partial class Sprintra {
     /// <summary>
-    ///  Required designer variable.
+    /// Required designer variable.
     /// </summary>
     private System.ComponentModel.IContainer components = null;
 
     /// <summary>
-    ///  Clean up any resources being used.
+    /// Clean up any resources being used.
     /// </summary>
     /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
     protected override void Dispose(bool disposing) {
@@ -19,14 +19,21 @@
     #region Windows Form Designer generated code
 
     /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
-      components = new System.ComponentModel.Container();
+      SuspendLayout();
+      // 
+      // Sprintra
+      // 
+      AutoScaleDimensions = new SizeF(7F, 15F);
       AutoScaleMode = AutoScaleMode.Font;
       ClientSize = new Size(800, 450);
-      Text = "Form1";
+      Name = "Sprintra";
+      StartPosition = FormStartPosition.CenterScreen;
+      Text = "MainForm";
+      ResumeLayout(false);
     }
 
     #endregion
