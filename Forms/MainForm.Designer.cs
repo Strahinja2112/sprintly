2757ac7bb4669900cfe9792102628061a91d9dba
Strahinja Pavićević <pavicevicstrahinja99@gmail.com>
2026-04-22 03:42:18 +0200

    DODAO SAM GOMILU STVARI SPAVA MI SE

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -25,58 +25,66 @@
     private void InitializeComponent() {
       panel1 = new Panel();
       PanelDashboard = new Panel();
-      bigLabel1 = new ReaLTaiizor.Controls.BigLabel();
+      LabelDashboard = new ReaLTaiizor.Controls.BigLabel();
       iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
       ButtonLogout = new ReaLTaiizor.Controls.AloneButton();
-      LabelUserRole = new ReaLTaiizor.Controls.BigLabel();
+      LabelUserType = new ReaLTaiizor.Controls.BigLabel();
       LabelUserName = new ReaLTaiizor.Controls.BigLabel();
+      panel2 = new Panel();
+      bigLabel1 = new ReaLTaiizor.Controls.BigLabel();
+      iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
       panel1.SuspendLayout();
       PanelDashboard.SuspendLayout();
       ((System.ComponentModel.ISupportInitialize)iconPictureBox1).BeginInit();
+      panel2.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)iconPictureBox2).BeginInit();
       SuspendLayout();
       // 
       // panel1
       // 
       panel1.BackColor = SystemColors.ControlLight;
+      panel1.Controls.Add(panel2);
       panel1.Controls.Add(PanelDashboard);
       panel1.Controls.Add(ButtonLogout);
-      panel1.Controls.Add(LabelUserRole);
+      panel1.Controls.Add(LabelUserType);
       panel1.Controls.Add(LabelUserName);
       panel1.Dock = DockStyle.Left;
       panel1.Location = new Point(0, 0);
       panel1.Name = "panel1";
-      panel1.Size = new Size(175, 450);
+      panel1.Size = new Size(190, 450);
       panel1.TabIndex = 3;
       // 
       // PanelDashboard
       // 
-      PanelDashboard.Controls.Add(bigLabel1);
+      PanelDashboard.Controls.Add(LabelDashboard);
       PanelDashboard.Controls.Add(iconPictureBox1);
       PanelDashboard.Dock = DockStyle.Top;
       PanelDashboard.Location = new Point(0, 0);
       PanelDashboard.Name = "PanelDashboard";
-      PanelDashboard.Size = new Size(175, 50);
+      PanelDashboard.Size = new Size(190, 50);
       PanelDashboard.TabIndex = 8;
-      PanelDashboard.Paint += PanelDashboard_Paint;
+      PanelDashboard.Click += PanelDashboard_Click;
       // 
-      // bigLabel1
+      // LabelDashboard
       // 
-      bigLabel1.BackColor = Color.Transparent;
-      bigLabel1.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
-      bigLabel1.ForeColor = Color.FromArgb(45, 45, 45);
-      bigLabel1.Location = new Point(42, 6);
-      bigLabel1.Name = "bigLabel1";
-      bigLabel1.Size = new Size(133, 41);
-      bigLabel1.TabIndex = 6;
-      bigLabel1.Text = "Dashboard";
-      bigLabel1.TextAlign = ContentAlignment.MiddleLeft;
+      LabelDashboard.BackColor = Color.Transparent;
+      LabelDashboard.Dock = DockStyle.Right;
+      LabelDashboard.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+      LabelDashboard.ForeColor = Color.FromArgb(45, 45, 45);
+      LabelDashboard.Location = new Point(50, 0);
+      LabelDashboard.Name = "LabelDashboard";
+      LabelDashboard.Size = new Size(140, 50);
+      LabelDashboard.TabIndex = 6;
+      LabelDashboard.Text = "Dashboard";
+      LabelDashboard.TextAlign = ContentAlignment.MiddleLeft;
       // 
       // iconPictureBox1
       // 
       iconPictureBox1.BackColor = SystemColors.ControlLight;
-      iconPictureBox1.ForeColor = SystemColors.ControlText;
+      iconPictureBox1.Enabled = false;
+      iconPictureBox1.ForeColor = Color.FromArgb(45, 45, 45);
       iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Dashboard;
-      iconPictureBox1.IconColor = SystemColors.ControlText;
+      iconPictureBox1.IconColor = Color.FromArgb(45, 45, 45);
       iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
       iconPictureBox1.IconSize = 35;
       iconPictureBox1.Location = new Point(9, 12);
@@ -95,22 +103,22 @@
       ButtonLogout.ForeColor = Color.FromArgb(124, 133, 142);
       ButtonLogout.Location = new Point(9, 414);
       ButtonLogout.Name = "ButtonLogout";
-      ButtonLogout.Size = new Size(157, 26);
+      ButtonLogout.Size = new Size(172, 26);
       ButtonLogout.TabIndex = 7;
       ButtonLogout.Text = "Odjavi se";
       ButtonLogout.Click += ButtonLogout_Click;
       // 
-      // LabelUserRole
+      // LabelUserType
       // 
-      LabelUserRole.AutoSize = true;
-      LabelUserRole.BackColor = Color.Transparent;
-      LabelUserRole.Font = new Font("Segoe UI", 9F);
-      LabelUserRole.ForeColor = Color.FromArgb(80, 80, 80);
-      LabelUserRole.Location = new Point(9, 391);
-      LabelUserRole.Name = "LabelUserRole";
-      LabelUserRole.Size = new Size(56, 15);
-      LabelUserRole.TabIndex = 6;
-      LabelUserRole.Text = "User Role";
+      LabelUserType.AutoSize = true;
+      LabelUserType.BackColor = Color.Transparent;
+      LabelUserType.Font = new Font("Segoe UI", 9F);
+      LabelUserType.ForeColor = Color.FromArgb(80, 80, 80);
+      LabelUserType.Location = new Point(9, 391);
+      LabelUserType.Name = "LabelUserType";
+      LabelUserType.Size = new Size(57, 15);
+      LabelUserType.TabIndex = 6;
+      LabelUserType.Text = "User Type";
       // 
       // LabelUserName
       // 
@@ -123,6 +131,46 @@
       LabelUserName.Size = new Size(127, 30);
       LabelUserName.TabIndex = 5;
       LabelUserName.Text = "User Name";
+      // 
+      // panel2
+      // 
+      panel2.Controls.Add(bigLabel1);
+      panel2.Controls.Add(iconPictureBox2);
+      panel2.Dock = DockStyle.Top;
+      panel2.Location = new Point(0, 50);
+      panel2.Name = "panel2";
+      panel2.Size = new Size(190, 50);
+      panel2.TabIndex = 9;
+      // 
+      // bigLabel1
+      // 
+      bigLabel1.BackColor = Color.Transparent;
+      bigLabel1.Dock = DockStyle.Right;
+      bigLabel1.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+      bigLabel1.ForeColor = Color.FromArgb(45, 45, 45);
+      bigLabel1.Location = new Point(50, 0);
+      bigLabel1.Name = "bigLabel1";
+      bigLabel1.Size = new Size(140, 50);
+      bigLabel1.TabIndex = 6;
+      bigLabel1.Text = "Dashboard";
+      bigLabel1.TextAlign = ContentAlignment.MiddleLeft;
+      // 
+      // iconPictureBox2
+      // 
+      iconPictureBox2.BackColor = SystemColors.ControlLight;
+      iconPictureBox2.Enabled = false;
+      iconPictureBox2.ForeColor = Color.FromArgb(45, 45, 45);
+      iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.Dashboard;
+      iconPictureBox2.IconColor = Color.FromArgb(45, 45, 45);
+      iconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+      iconPictureBox2.IconSize = 35;
+      iconPictureBox2.Location = new Point(9, 12);
+      iconPictureBox2.Name = "iconPictureBox2";
+      iconPictureBox2.Padding = new Padding(20, 20, 0, 0);
+      iconPictureBox2.Size = new Size(35, 35);
+      iconPictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+      iconPictureBox2.TabIndex = 0;
+      iconPictureBox2.TabStop = false;
       // 
       // MainForm
       // 
@@ -140,16 +188,21 @@
       panel1.PerformLayout();
       PanelDashboard.ResumeLayout(false);
       ((System.ComponentModel.ISupportInitialize)iconPictureBox1).EndInit();
+      panel2.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)iconPictureBox2).EndInit();
       ResumeLayout(false);
     }
 
     #endregion
     private Panel panel1;
     private ReaLTaiizor.Controls.BigLabel LabelUserName;
-    private ReaLTaiizor.Controls.BigLabel LabelUserRole;
+    private ReaLTaiizor.Controls.BigLabel LabelUserType;
     private ReaLTaiizor.Controls.AloneButton ButtonLogout;
     private Panel PanelDashboard;
     private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+    private ReaLTaiizor.Controls.BigLabel LabelDashboard;
+    private Panel panel2;
     private ReaLTaiizor.Controls.BigLabel bigLabel1;
+    private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
   }
 }
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -5,6 +5,22 @@ namespace Sprintra.Forms;
 public partial class MainForm : Form {
   public MainForm() {
     InitializeComponent();
+
+    //using var db = new AppDbContext();
+
+    //var employee = new Employee() {
+    //  FirstName = "Strahinja",
+    //  LastName = "Prezime", // Dodaj pravo prezime
+    //  Email = "strahinja@sprintra.com",
+    //  Username = "strahinja",
+    //  PasswordHash = AuthService.HashPassword("123"),
+    //  HireDate = DateTime.Now,
+    //  Status = "Active",
+    //  Type = EmployeeType.Developer
+    //};
+
+    //db.Employees.Add(employee);
+    //db.SaveChanges();
   }
 
   private void MainForm_Load(object sender, EventArgs e) {
@@ -12,8 +28,10 @@ public partial class MainForm : Form {
       Close();
     }
 
+    LabelDashboard.Click += PanelDashboard_Click;
+
     LabelUserName.Text = "@" + AuthService.CurrentUser.Username;
-    LabelUserRole.Text = AuthService.CurrentUser.TeamRole?.ToString() ?? "N/A";
+    LabelUserType.Text = AuthService.CurrentUser.Type.ToString();
   }
 
 
@@ -21,7 +39,7 @@ public partial class MainForm : Form {
     AuthService.Logout(this);
   }
 
-  private void PanelDashboard_Paint(object sender, PaintEventArgs e) {
-
+  private void PanelDashboard_Click(object sender, EventArgs e) {
+    MessageBox.Show("MEMTASD");
   }
 }
